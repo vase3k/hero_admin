dbf4a8ade24a22dcfe4df849106f15d6b9adb9e3
Vasiliy Vinogradov <vase3k@gmail.com>
2025-08-15 23:28:04 +0300

    configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -12,29 +11,10 @@ const stringMiddleware = () => next => action => {
     return next(action);
 };
 
-const enchancer =
-    createStore =>
-    (...args) => {
-        const store = createStore(...args);
-
-        const oldDispatch = store.dispatch;
-        store.dispatch = action => {
-            if (typeof action === 'string') {
-                return oldDispatch({
-                    type: action,
-                });
-            }
-            return oldDispatch(action);
-        };
-        return store;
-    };
-
-const store = createStore(
-    combineReducers({ heroes, filters }),
-    compose(
-        applyMiddleware(thunk, stringMiddleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+const store = configureStore({
+    reducer: { heroes, filters },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
