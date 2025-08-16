2eee7bd11959638576dfeddc28020ff309434eb6
Vasiliy Vinogradov <vase3k@gmail.com>
2025-08-16 11:57:55 +0300

    createReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,76 +1,11 @@
 import { createAction } from '@reduxjs/toolkit';
 
-// export const heroesFetching = () => {
-//     return {
-//         type: 'HEROES_FETCHING',
-//     };
-// };
-
 export const heroesFetching = createAction('HEROES_FETCHING');
-
-// export const heroesFetched = heroes => {
-//     return {
-//         type: 'HEROES_FETCHED',
-//         payload: heroes,
-//     };
-// };
-
 export const heroesFetched = createAction('HEROES_FETCHED');
-
-// export const heroesFetchingError = () => {
-//     return {
-//         type: 'HEROES_FETCHING_ERROR',
-//     };
-// };
-
 export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
-
-// export const heroDeleted = id => {
-//     return {
-//         type: 'HERO_DELETED',
-//         payload: id,
-//     };
-// };
-
 export const heroDeleted = createAction('HERO_DELETED');
-
-// export const heroCreated = hero => {
-//     return {
-//         type: 'HERO_CREATED',
-//         payload: hero,
-//     };
-// };
-
 export const heroCreated = createAction('HERO_CREATED');
-
-// export const filtersFetching = () => {
-//     return { type: 'FILTERS_FETCHING' };
-// };
-
 export const filtersFetching = createAction('FILTERS_FETCHING');
-
-// export const filtersFetched = filters => {
-//     return {
-//         type: 'FILTERS_FETCHED',
-//         payload: filters,
-//     };
-// };
-
 export const filtersFetched = createAction('FILTERS_FETCHED');
-
-// export const filtersFetchingError = () => {
-//     return {
-//         type: 'FILTERS_FETCHING_ERROR',
-//     };
-// };
-
 export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
-
-// export const activeFilterChanged = filter => {
-//     return {
-//         type: 'ACTIVE_FILTER_CHANGED',
-//         payload: filter,
-//     };
-// };
-
 export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED');
--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,35 +1,34 @@
+import { createReducer } from '@reduxjs/toolkit';
+
+import {
+    filtersFetching,
+    filtersFetched,
+    filtersFetchingError,
+    activeFilterChanged,
+} from '../actions';
+
 const initialState = {
     filters: [],
     filtersLoadingStatus: 'idle',
     activeFilter: 'all',
 };
 
-const filters = (state = initialState, action) => {
-    switch (action.type) {
-        case 'FILTERS_FETCHING':
-            return {
-                ...state,
-                filtersLoadingStatus: 'loading',
-            };
-        case 'FILTERS_FETCHED':
-            return {
-                ...state,
-                filters: action.payload,
-                filtersLoadingStatus: 'idle',
-            };
-        case 'FILTERS_FETCHING_ERROR':
-            return {
-                ...state,
-                filtersLoadingStatus: 'error',
-            };
-        case 'ACTIVE_FILTER_CHANGED':
-            return {
-                ...state,
-                activeFilter: action.payload,
-            };
-        default:
-            return state;
-    }
-};
+const filters = createReducer(initialState, builder => {
+    builder
+        .addCase(filtersFetching, state => {
+            state.filtersLoadingStatus = 'loading';
+        })
+        .addCase(filtersFetched, (state, action) => {
+            state.filtersLoadingStatus = 'idle';
+            state.filters = action.payload;
+        })
+        .addCase(filtersFetchingError, state => {
+            state.filtersLoadingStatus = 'error';
+        })
+        .addCase(activeFilterChanged, (state, action) => {
+            state.activeFilter = action.payload;
+        })
+        .addDefaultCase(() => {});
+});
 
 export default filters;
